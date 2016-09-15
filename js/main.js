01042f7692798bb3170a9ce0abafd7b9829f0b86
MarisolenlaWeb <marcarrimor@gmail.com>
2016-09-15 00:33:41 -0500

    Add html, css and js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+  $('.button-collapse').sideNav({
+      menuWidth: 350, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
